9512508d7ee7fd2f3fb989824788b6603bdcfb23
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-20 16:05:00 -0400

    Update test-plone-export

--- project.mk
+++ project.mk
@@ -1,2 +1,2 @@
 PROJECT_NAME := test-plone-export
-PIP_INSTALL_PLONE_CONSTRAINTS = https://dist.plone.org/release/5.1-latest/constraints.txt
+# PIP_INSTALL_PLONE_CONSTRAINTS = https://dist.plone.org/release/5.1-latest/constraints.txt
