4ae5f07b300f033931ce4e3769044f01049ad775
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-21 16:42:39 -0400

    Update test-plone-export

--- project.mk
+++ project.mk
@@ -1,4 +1,15 @@
 # Custom Makefile
 # Add your custom makefile commands here
 #
-# PROJECT_NAME := my-new-project
+PROJECT_NAME := test-plone-export
+# PIP_INSTALL_PLONE_CONSTRAINTS = https://dist.plone.org/release/5.1-latest/constraints.txt
+
+test:
+	$(MAKE) clean git-commit-clean git-ignore git-commit-ignore plone-install plone-instance
+	$(MAKE) git-commit-init
+
+dump:
+	.venv/bin/zconsole run backend/etc/zope.conf  export.py
+
+d:
+	$(MAKE) dump
