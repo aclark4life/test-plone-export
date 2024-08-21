795bcd6202ca91c638506fa5ae6ba18748daf697
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-21 16:51:21 -0400

    Lint

--- create_plone_site.py
+++ create_plone_site.py
@@ -1,8 +1,9 @@
 from Products.CMFPlone.factory import addPloneSite
 from transaction import commit
 
+
 def create_plone_site(app):
-    site_id = 'Plone'
+    site_id = "Plone"
 
     # Check if the site already exists
     if site_id not in app.objectIds():
@@ -16,7 +17,8 @@ def create_plone_site(app):
     else:
         print(f"Plone site '{site_id}' already exists.")
 
+
 # This is the entry point for zconsole run
-if __name__ == '__main__':
-    app = globals().get('app')
+if __name__ == "__main__":
+    app = globals().get("app")
     create_plone_site(app)
