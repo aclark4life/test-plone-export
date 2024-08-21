1a9fff1f04aeec3d68f1a7003a9244248920e764
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-21 16:29:28 -0400

    Update test-plone-export

--- project.mk
+++ project.mk
@@ -2,4 +2,4 @@ PROJECT_NAME := test-plone-export
 # PIP_INSTALL_PLONE_CONSTRAINTS = https://dist.plone.org/release/5.1-latest/constraints.txt
 
 test:
-	$(MAKE) clean git-commit-clean git-ignore git-commit plone-install plone-instance git-commit-init
+	$(MAKE) clean git-commit-clean git-ignore git-commit-ignore plone-install plone-instance git-commit-init
