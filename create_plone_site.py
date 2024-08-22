13286600c38e130ec670bb1f9fa2612780d3e1ec
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-22 12:21:25 -0400

    Clean

--- create_plone_site.py
+++ create_plone_site.py
@@ -13,7 +13,6 @@ def create_plone_site(app, ui_type="classic"):
             add_on_profiles = [
                 "Products.CMFPlone:plone",  # Classic Plone UI
                 "plonetheme.barceloneta:default",  # Default theme for classic UI
-                "plone.app.contenttypes:default",  # Default content types
             ]
             title = "My Classic Plone Site"
         elif ui_type == "volto":
